be13591e5876a27aa9474e0d5036cc9a5c1345ec
Imad Bin Asad <105483337+imad10902@users.noreply.github.com>
2023-06-23 17:13:40 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add-task/', views.addTask, name='add-task'),
-    path('update-task/<str:pk>', views.updateTask, name='update-task'),
-    path('delete-task/<str:pk>', views.deleteTask, name='delete-task'),
-    path('delete-task/', views.deleteAll, name='delete-all'),
+    path('api/todo/add-task/', views.addTask, name='add-task'),
+    path('api/todo/update-task/<str:pk>', views.updateTask, name='update-task'),
+    path('api/todo/delete-task/<str:pk>', views.deleteTask, name='delete-task'),
+    path('api/todo/delete-tasks/', views.deleteAll, name='delete-all'),
     path('login-page/', views.loginPage, name='login-page'),
     path('register-page/', views.registerPage, name='register-page'),
     path('logout-user/', views.logoutUser, name='logout-user'),
